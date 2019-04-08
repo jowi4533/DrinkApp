71cee60a892dcccc2c5e21b1c2acfa80caaf2672
Jonas Wikström <jonaswikstrom@student-212-122.eduroam.uu.se>
2019-04-08 15:31:17 +0200

    navigationTabBar v1.0.0

--- Jojax/App.js
+++ Jojax/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
+import Drinks from './screens/Drinks'
+import Explore from './screens/Explore'
+import More from './screens/More'
+import MyPage from './screens/MyPage'
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//           <Text>hej kevin</Text>
+//       </View>
+//     );
+//   }
+// }
+const TabOptions = createBottomTabNavigator({
+  Explore:{
+    screen: Explore
+  },
+  Drinks:{
+    screen: Drinks
+  },
+  MyPage:{
+    screen: MyPage
+  },
+  More:{
+    screen: More
   }
-}
+
+})
+
+const AppContainer = createAppContainer(TabOptions);
+export default AppContainer;
+
 
 const styles = StyleSheet.create({
   container: {
