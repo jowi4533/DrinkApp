ee7c7be1bfb7c09bdd399e9a80af2d57232a66fd
Jonas Wikström <jonaswikstrom@student-216-105.eduroam.uu.se>
2019-05-24 12:53:03 +0200

    notes

---
+++ Jojax/components/DeleteNoteButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+const DeleteNoteButton =( { onPress } ) => {
+
+  return(
+<TouchableOpacity onPress= {onPress} style = {styles.DeleteNoteContainer}>
+  <FontAwesome
+    name={'trash-o'}
+    size={40}
+    color={'rgb(105,105,105)'}
+    />
+
+</TouchableOpacity>
+
+
+  );
+
+  }
+  export default DeleteNoteButton;
+
+
+
+const styles = StyleSheet.create({
+  DeleteNoteContainer:{
+    backgroundColor: 'pink',
+    padding: 10,
+    paddingBottom: -10,
+    marginRight: 10
+  }
+
+
+
+});
